5476a4ba8a5b480ab5c97eb8c2b0755795c2bab8
Rubenvill0811 <rubenvill0811@gmail.com>
2024-06-22 15:31:35 -0700

    Tried to get the app to start, but I keep gettin an access denied error.

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -2,14 +2,12 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
-      dialect: 'mysql',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  host: process.env.DB_HOST || 'localhost', // Use DB_HOST if provided, otherwise default to localhost
+  dialect: 'mysql',
+  dialectOptions: {
+    decimalNumbers: true,
+  },
+});
 
 module.exports = sequelize;
--- Develop/server.js
+++ Develop/server.js
@@ -4,7 +4,8 @@ const sequelize = require('./config/connection');
 // import sequelize connection
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+// const PORT = process.env.PORT || 3001;
+const PORT = 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
